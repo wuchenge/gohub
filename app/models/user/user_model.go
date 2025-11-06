2b324c685245142469ad1ea2eb02e71cfb5bb9e2
wuchenge <ligui.wuchenge@gmail.com>
2025-11-06 09:24:28 +0800

    feat: 通过手机找回密码

--- app/models/user/user_model.go
+++ app/models/user/user_model.go
@@ -29,3 +29,7 @@ func (userModel *User) ComparePassword(_password string) bool {
     return hash.BcryptCheck(_password, userModel.Password)
 }
 
+func (userModel *User) Save() (rowsAffected int64) {
+    result := database.DB.Save(&userModel)
+    return result.RowsAffected
+}
